374f52f0743b257b92bba0bd2490a14fcdcec3f0
Fred <fj.schmitz@student.maastrichtuniversity.nl>
2022-11-03 14:45:40 +0100

    Update MenuScreen.java

--- core/src/com/mygdx/game/screens/MenuScreen.java
+++ core/src/com/mygdx/game/screens/MenuScreen.java
@@ -1,14 +1,11 @@
 package com.mygdx.game.screens;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -17,21 +14,21 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FillViewport;
-import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.mygdx.game.Omega;
-import com.mygdx.game.screens.GameScreen;
-
 import java.util.ArrayList;
 
 /**
- *MenuScreen is the first screen the user will see when starting the game,in this file he can choose between multiples
+ * MenuScreen is the first screen the user will see when starting the game,in
+ * this file he can choose between multiples
  * maps and options before starting the game
  */
 public class MenuScreen implements Screen {
 
     private Omega game;
-    private boolean click = false;
-    private boolean clickAI = false;
+    private TextButton BVB;
+    private boolean clickPvP = false;
+    private boolean clickBVB = false;
+    private boolean clickPvB = false;
     private boolean clickInstruction = false;
     private Stage stage;
     private SpriteBatch mainBatch;
@@ -44,7 +41,7 @@ public class MenuScreen implements Screen {
     private TextButton mapSimple;
     private TextButton mapBug;
     private TextButton instructionButton;
-    public static int mapChoice =0 ;
+    public static int mapChoice = 0;
     private ArrayList<TextButton> listOfMapButtons;
 
     /**
@@ -59,21 +56,26 @@ public class MenuScreen implements Screen {
         omegaSymbol = new Texture(Gdx.files.internal("omegaSymbol.png"));
         PVP = new TextButton("Play: 2 Player", menuSkin);
         PVP.setColor(Color.BLACK);
-        PVP.setPosition(445, 270);
+        PVP.setPosition(325, 270);
         PVP.setSize(200, 100);
 
         PVAI = new TextButton("Play: AI", menuSkin);
         PVAI.setColor(Color.BLACK);
-        PVAI.setPosition(645, 270);
+        PVAI.setPosition(525, 270);
         PVAI.setSize(200, 100);
 
         listOfMapButtons = new ArrayList<>();
+
+        BVB = new TextButton("Play: Bot vs Bot", menuSkin);
+        BVB.setColor(Color.BLACK);
+        BVB.setPosition(725, 270);
+        BVB.setSize(200, 100);
 
         mapDefault = new TextButton("Default", menuSkin);
         mapDefault.setColor(Color.BLACK);
         mapDefault.setPosition(445, 100);
         mapDefault.setSize(200, 100);
-        mapDefault.setColor(new Color(0.5f,0.5f,0.5f,1f));
+        mapDefault.setColor(new Color(0.5f, 0.5f, 0.5f, 1f));
 
         mapSnowflake = new TextButton("Snowflake", menuSkin);
         mapSnowflake.setColor(Color.BLACK);
@@ -97,9 +99,10 @@ public class MenuScreen implements Screen {
 
         instructionButton = new TextButton("Game Instructions", menuSkin);
         instructionButton.setColor(Color.RED);
-        instructionButton.setPosition(45,25);
-        instructionButton.setSize(150,50);
+        instructionButton.setPosition(45, 25);
+        instructionButton.setSize(150, 50);
 
+        stage.addActor(BVB);
         stage.addActor(PVP);
         stage.addActor(PVAI);
         stage.addActor(mapDefault);
@@ -111,61 +114,72 @@ public class MenuScreen implements Screen {
 
     @Override
     /**
-     *Render method render the screen every x times to put new information on the screen when action occur
+     * Render method render the screen every x times to put new information on the
+     * screen when action occur
      */
     public void render(float delta) {
-        ScreenUtils.clear(0.90f,1.00f,1.00f, 1);
+        ScreenUtils.clear(0.90f, 1.00f, 1.00f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         PVP.addListener(new ClickListener() {
             public void touchUp(InputEvent e, float x, float y, int point, int button) {
-                click = true;
+                clickPvP = true;
             }
         });
 
-        if (click) {
+        if (clickPvP) {
             this.dispose();
-            game.setScreen(new LoadingScreen(game,false));
+            game.setScreen(new LoadingScreen(game, false));
         }
 
         PVAI.addListener(new ClickListener() {
             public void touchUp(InputEvent e, float x, float y, int point, int button) {
-                clickAI = true;
+                clickPvB = true;
             }
         });
 
-        if (clickAI) {
+        if (clickBVB) {
             this.dispose();
-            game.setScreen(new LoadingScreen(game,true));
+            game.setScreen(new LoadingScreen(game, true));
         }
 
+        BVB.addListener(new ClickListener() {
+            public void touchUp(InputEvent e, float x, float y, int point, int button) {
+                clickBVB = true;
+            }
+        });
+
+        if (clickPvB) {
+            this.dispose();
+            game.setScreen(new LoadingScreen(game, true));
+        }
 
         mapSnowflake.addListener(new ClickListener() {
             public void touchUp(InputEvent e, float x, float y, int point, int button) {
-                mapChoice =1;
+                mapChoice = 1;
                 resetButtons();
-                mapSnowflake.setColor(new Color(0.5f,0.5f,0.5f,1f));
+                mapSnowflake.setColor(new Color(0.5f, 0.5f, 0.5f, 1f));
             }
         });
         mapSimple.addListener(new ClickListener() {
             public void touchUp(InputEvent e, float x, float y, int point, int button) {
-                mapChoice =2;
+                mapChoice = 2;
                 resetButtons();
-                mapSimple.setColor(new Color(0.5f,0.5f,0.5f,1f));
+                mapSimple.setColor(new Color(0.5f, 0.5f, 0.5f, 1f));
             }
         });
         mapDefault.addListener(new ClickListener() {
             public void touchUp(InputEvent e, float x, float y, int point, int button) {
-                mapChoice =0;
+                mapChoice = 0;
                 resetButtons();
-                mapDefault.setColor(new Color(0.5f,0.5f,0.5f,1f));
+                mapDefault.setColor(new Color(0.5f, 0.5f, 0.5f, 1f));
             }
         });
         mapBug.addListener(new ClickListener() {
             public void touchUp(InputEvent e, float x, float y, int point, int button) {
-                mapChoice =3;
+                mapChoice = 3;
                 resetButtons();
-                mapBug.setColor(new Color(0.5f,0.5f,0.5f,1f));
+                mapBug.setColor(new Color(0.5f, 0.5f, 0.5f, 1f));
             }
         });
 
@@ -175,8 +189,8 @@ public class MenuScreen implements Screen {
             }
         });
 
-        if (clickInstruction){
-            //System.out.println("BUtton clicked");
+        if (clickInstruction) {
+            // System.out.println("BUtton clicked");
             this.dispose();
             game.setScreen(new InstructionScreen(game));
         }
@@ -193,7 +207,7 @@ public class MenuScreen implements Screen {
         game.font.getData().setScale(3);
         game.font.draw(game.mainBatch, "OMEGA : Java edition", 430, 450);
         game.font.draw(game.mainBatch, "Map Selection", 500, 250);
-        game.mainBatch.draw(omegaSymbol, 525, 475,225f,225f);
+        game.mainBatch.draw(omegaSymbol, 525, 475, 225f, 225f);
 
         game.mainBatch.end();
         game.sr.end();
@@ -202,9 +216,9 @@ public class MenuScreen implements Screen {
     /**
      * the method put all the different buttons in the same organized colour
      */
-    public void resetButtons(){
-        for (TextButton tb:listOfMapButtons){
-            tb.setColor(new Color(0f,0f,0f,1f));
+    public void resetButtons() {
+        for (TextButton tb : listOfMapButtons) {
+            tb.setColor(new Color(0f, 0f, 0f, 1f));
         }
     }
 
