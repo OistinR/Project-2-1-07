d1f8f6d81b100399d2b821bcc5fdca2be77a8c77
Data_ <oistinrutledge1@gmail.com>
2022-11-03 19:20:44 +0100

    disabled text overlay

--- core/src/com/mygdx/game/coordsystem/Hexagon.java
+++ core/src/com/mygdx/game/coordsystem/Hexagon.java
@@ -74,7 +74,7 @@ public class Hexagon implements Cloneable {
         this.checked = false;
 
         //this enables the fitness score being rendered over the hexagon
-        DEBUG = true;
+        DEBUG = false;
 
         hexSprite = new Sprite(blankTileTexture,50,50);
         hexSprite.setSize(50,50);
