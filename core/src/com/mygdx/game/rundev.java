ee0bd4bbeb9b024fe5398900f499b32458552f68
Fred <fj.schmitz@student.maastrichtuniversity.nl>
2022-12-10 12:53:03 +0100

    added experiments folder to the branch

--- core/src/com/mygdx/game/rundev.java
+++ core/src/com/mygdx/game/rundev.java
@@ -23,7 +23,7 @@ import com.mygdx.game.scoringsystem.ScoringEngine;
 import com.mygdx.game.screens.GameScreen;
 
 public class rundev {
-    public enum state{
+    public enum state {
         P1P1,
         P1P2,
         P1P3,
@@ -46,23 +46,23 @@ public class rundev {
     private double finalWinRate;
 
     public void init() {
-        //Initiate variables
-        round=1;
-        totalnumgames=100;
-        gamefinished=false;
-		field = new ArrayList<>();
+        // Initiate variables
+        round = 1;
+        totalnumgames = 100;
+        gamefinished = false;
+        field = new ArrayList<>();
         SEngine = new ScoringEngine();
         bot1wins = new ArrayList<>();
         bot2wins = new ArrayList<>();
         draws = new ArrayList<>();
 
-        //Initiate fieldsize
-        fieldsize=2;
+        // Initiate fieldsize
+        fieldsize = 4;
 
-        //Create field and initiate bots
+        // Create field and initiate bots
 
-        //botpone = new TreeBot(Hexagon.state.RED,Hexagon.state.BLUE);
-        botpone = new TreeBot(Hexagon.state.RED,Hexagon.state.BLUE);
+        // botpone = new TreeBot(Hexagon.state.RED,Hexagon.state.BLUE);
+        botpone = new TreeBot(Hexagon.state.RED, Hexagon.state.BLUE);
         botptwo = new RandomBot();
         createHexagonFieldDefault();
     }
@@ -71,56 +71,59 @@ public class rundev {
         double winperc1;
         double winperc2;
         double winpercd;
-        int i=1;
-        while(i<=totalnumgames) {
-            while(!gamefinished) {
+        int i = 1;
+        while (i <= totalnumgames) {
+            while (!gamefinished) {
                 updateState();
                 makeMove();
                 updateState();
-            } 
-            if(i%10==0) {
-                System.out.println(i+" games simulated.");
+            }
+            if (i % 10 == 0) {
+                System.out.println(i + " games simulated.");
             }
 
-            round=1;
-            gamefinished=false;
+            round = 1;
+            gamefinished = false;
             field = new ArrayList<>();
             SEngine = new ScoringEngine();
             createHexagonFieldDefault();
 
             i++;
         }
-        //TODO issue: the tree cant tell when game is over so its possible that infinite loops can occur.
-//        Tree tr = new Tree(6,5);
-//        // Storage is a massive issue, larger map sizes means lower depth/widths.
-//        long runtime=0L;
-//        long startTime = System.nanoTime();
-//        tr.generateTree(field, GameScreen.state.P2P1,false);
-//        long endTime = System.nanoTime();
-//        long duration = (endTime - startTime);
-//        runtime += duration/10000000;
-//        System.out.println("\nruntime: "+ ((double)(runtime))/100+" seconds(i think)");
-//
-//        System.out.println(tr.displayTree(false));
-//
-//        System.out.println("bots assessment of board: "+ tr.getNodes().get(0).getCombinedScore());
-//
-//        double maxScore = 0;
-//        for (Node n0: tr.getNodes().get(0).getChildArray()) {
-//            if(maxScore<n0.getCombinedScore()){
-//                maxScore = n0.getCombinedScore();
-//            }
-//            System.out.println(n0);
-//        }
-//        System.out.println("max score is: "+ maxScore);
+        // TODO issue: the tree cant tell when game is over so its possible that
+        // infinite loops can occur.
+        // Tree tr = new Tree(6,5);
+        // // Storage is a massive issue, larger map sizes means lower depth/widths.
+        // long runtime=0L;
+        // long startTime = System.nanoTime();
+        // tr.generateTree(field, GameScreen.state.P2P1,false);
+        // long endTime = System.nanoTime();
+        // long duration = (endTime - startTime);
+        // runtime += duration/10000000;
+        // System.out.println("\nruntime: "+ ((double)(runtime))/100+" seconds(i
+        // think)");
+        //
+        // System.out.println(tr.displayTree(false));
+        //
+        // System.out.println("bots assessment of board: "+
+        // tr.getNodes().get(0).getCombinedScore());
+        //
+        // double maxScore = 0;
+        // for (Node n0: tr.getNodes().get(0).getChildArray()) {
+        // if(maxScore<n0.getCombinedScore()){
+        // maxScore = n0.getCombinedScore();
+        // }
+        // System.out.println(n0);
+        // }
+        // System.out.println("max score is: "+ maxScore);
 
-        winperc1 = ((double)bot1wins.size()/(double)totalnumgames)*100;
-        winperc2 = ((double)bot2wins.size()/(double)totalnumgames)*100;
-        winpercd = ((double)draws.size()/(double)totalnumgames)*100;
+        winperc1 = ((double) bot1wins.size() / (double) totalnumgames) * 100;
+        winperc2 = ((double) bot2wins.size() / (double) totalnumgames) * 100;
+        winpercd = ((double) draws.size() / (double) totalnumgames) * 100;
 
-        System.out.println("Bot 1 number of wins: "+bot1wins.size()+ " Win percentage: "+ winperc1+ " %" );
-        System.out.println("Bot 2 number of wins: "+bot2wins.size()+ " Win percentage: "+ winperc2+ " %" );
-        System.out.println("Number of draws: "+draws.size()+ " Draw percentage: "+ winpercd+" %");
+        System.out.println("Bot 1 number of wins: " + bot1wins.size() + " Win percentage: " + winperc1 + " %");
+        System.out.println("Bot 2 number of wins: " + bot2wins.size() + " Win percentage: " + winperc2 + " %");
+        System.out.println("Number of draws: " + draws.size() + " Draw percentage: " + winpercd + " %");
         this.finalWinRate = winperc1;
     }
 
@@ -130,24 +133,24 @@ public class rundev {
             for (int r = fieldsize; r >= -fieldsize; r--) {
                 s = -q - r;
                 if (s <= fieldsize && s >= -fieldsize) {
-                    field.add(new Hexagon(q, r, 50,0,0));
+                    field.add(new Hexagon(q, r, 50, 0, 0));
                 }
             }
         }
     }
 
     public void updateState() {
-        int numhex = numHex() - 4*(round-1);
+        int numhex = numHex() - 4 * (round - 1);
 
-        if(field.size()-(numhex+(4*(round-1)))<4) {
+        if (field.size() - (numhex + (4 * (round - 1))) < 4) {
             gameFinish();
         }
     }
 
     public int numHex() {
-        int num=0;
-        for(Hexagon h:field) {
-            if((h.getMyState()!=Hexagon.state.BLANK)) {
+        int num = 0;
+        for (Hexagon h : field) {
+            if ((h.getMyState() != Hexagon.state.BLANK)) {
                 num++;
             }
         }
@@ -161,45 +164,43 @@ public class rundev {
 
     public void gameFinish() {
         SEngine.calculate(field);
-        int p1score=SEngine.getRedScore();
-        int p2score=SEngine.getBlueScore();
-        //System.out.println("GAME HAS ENDED");
-        //System.out.println("PLAYER 1 (RED) SCORE: "+p1score);
-        //System.out.println("PLAYER 2 (BLUE) SCORE: "+p2score);
-        if(p1score>p2score) {
-            //System.out.println("PLAYER 1 (RED) WON");
+        int p1score = SEngine.getRedScore();
+        int p2score = SEngine.getBlueScore();
+        // System.out.println("GAME HAS ENDED");
+        // System.out.println("PLAYER 1 (RED) SCORE: "+p1score);
+        // System.out.println("PLAYER 2 (BLUE) SCORE: "+p2score);
+        if (p1score > p2score) {
+            // System.out.println("PLAYER 1 (RED) WON");
             bot1wins.add(p1score);
-        } else if(p1score<p2score) {
-            //System.out.println("PLAYER 2 (BLUE) WON");
+        } else if (p1score < p2score) {
+            // System.out.println("PLAYER 2 (BLUE) WON");
             bot2wins.add(p2score);
         } else {
-            //System.out.println("THE GAME IS A DRAW");
+            // System.out.println("THE GAME IS A DRAW");
             draws.add(p1score);
         }
-        gamefinished=true;
+        gamefinished = true;
     }
 
-    public ArrayList<Hexagon> getField(){
+    public ArrayList<Hexagon> getField() {
         return this.field;
     }
 
-    public double getWinRate(){
+    public double getWinRate() {
         return this.finalWinRate;
     }
 
-    public void setField(ArrayList<Hexagon> field){
+    public void setField(ArrayList<Hexagon> field) {
         ArrayList<Hexagon> clone = new ArrayList<>();
-        try{
-            for(Hexagon hex:field){
+        try {
+            for (Hexagon hex : field) {
                 clone.add(hex.clone());
             }
-        }
-        catch(Exception e){
+        } catch (Exception e) {
 
         }
         this.field = field;
     }
-
 
     public static void main(String[] args) {
         rundev dev = new rundev();
