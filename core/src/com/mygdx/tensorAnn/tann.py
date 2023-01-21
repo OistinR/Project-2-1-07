4a0933099d4202bc73c1d4fecc4dde9a63427bdd
Data_ <oistinrutledge1@gmail.com>
2023-01-21 14:19:21 +0100

    pre massive training session commit

--- core/src/com/mygdx/tensorAnn/tann.py
+++ core/src/com/mygdx/tensorAnn/tann.py
@@ -14,7 +14,7 @@ postMoves = []
 
 # opening the CSV file
 def read_Data():
-    with open("C:/Users/Oistín/Documents/Github/Project-2-1-07/core/src/trainingData2.csv", mode='r') as file:
+    with open("C:/Users/Oistín/Documents/Github/Project-2-1-07/core/src/trainingDataAll.csv", mode='r') as file:
         # reading the CSV file
         csvFile = csv.reader(file)
         # displaying the contents of the CSV file
@@ -36,7 +36,6 @@ def read_Data():
                     postgame.clear()
                     flag = True
                 elif flag:
-                    # print(pregame)
                     pregame.insert(pregame.__sizeof__(), float(num))
                 else:
                     postgame.insert(postgame.__sizeof__(), abs(float(num)))
@@ -44,18 +43,16 @@ def read_Data():
 
 
 def training():
-    read_Data()
-    #preMoves = []
-    #postMoves = []
+    # preMoves = []
+    # postMoves = []
 
     # X_train, X_test, Y_train, Y_test = train_test_split(preMoves, postMoves)
-    #
     # X_train.head()
     # # Y_train.head()
     # print(len(preMoves[0]))
     # print(len(preMoves[0][0]))
-    X = np.asarray(preMoves.copy())
 
+    X = np.asarray(preMoves.copy())
     Y = np.asarray(postMoves.copy())
 
     # X_train, X_test, Y_train, Y_split = train_test_split(X,Y, test_size=0.2)
@@ -66,32 +63,32 @@ def training():
     #     for list2 in list:
     #         print(list2)
 
-    # model = Sequential()
-    # model.add(Dense(units=38, activation= 'relu', input_dim = 38))
-    # model.add(Dense(units=220, activation='relu'))
-    # model.add(Dense(units=120, activation='relu')) # 96% relu relu softmax
-    # model.add(Dense(37, activation='softmax')) #96.8 tan tan  tan sofmax 0.05? on test data
-    #
-    # model.compile(loss='binary_crossentropy', optimizer="Adam", metrics='accuracy')
+    model = Sequential()
+    model.add(Dense(units=400, activation= 'relu', input_dim = 38))
+    model.add(Dense(37, activation='softmax')) #96.8 tan tan  tan sofmax 0.05? on test data
+    model.compile(loss='binary_crossentropy', optimizer="adam", metrics='accuracy')
+
     model = tf.keras.models.load_model('C:/Users/Oistín/Documents/Github/Project-2-1-07/core/src/com/mygdx/tensorAnn/model1')
 
-    model.fit(X,Y, epochs=100, batch_size=32)#batch = 5000 games for 100 epoch and 32 batch is 15min+
+    model.fit(X,Y, epochs=50, batch_size=32)#batch = 5000 games for 100 epoch and 32 batch is 15min+
 
     model.save('C:/Users/Oistín/Documents/Github/Project-2-1-07/core/src/com/mygdx/tensorAnn/model1')
     # print(model.evaluate(preMoves,postMoves))
     winsound.Beep(440, 500)
 
-def main():
 
+def main():
     read_Data()
-    # print(len(preMoves))
+
+    # # print(len(preMoves))
     # print(len(postMoves))
     # print(len(preMoves[0]))
     # print(len(postMoves[0]))
     # print(preMoves)
-    # # print(postMoves)
+    # print(postMoves)
     # training()
     # winsound.Beep(440, 500)
+
     new_model = tf.keras.models.load_model('C:/Users/Oistín/Documents/Github/Project-2-1-07/core/src/com/mygdx/tensorAnn/model1')
     print(new_model.evaluate(preMoves, postMoves))
 
@@ -100,27 +97,24 @@ def main():
     # Y = np.asarray(postMoves.copy())
     # predict: object = new_model.predict(preMoves)
     # j = 0
-    #
     # for array in predict:
     #     array[array.argmax()] = 1.0
     #     i = 0
     #     while i< len(array):
     #         if array[i] < 1.0:
     #             data = 0.0
-    #
     # print(predict[0])
     # print(predict[0].argmax())
     # print(predict[0])
     # winsound.Beep(440, 500)
     # print(accuracy_score(postMoves, predict))
-    # # maxindex = np.max(predict)
+    # maxindex = np.max(predict)
 
     # print(predict)
     # print(postMoves[0])
 
     # print("best index red:"+str(maxindex))
     # print("best index red:"+str(postMoves[0][predict.argmax()]))
-    #
 
 
 
